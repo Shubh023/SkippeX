cb8ed8d7fcf32f2bba06a4a07f23eda3eebd0970
Shubh023 <shubhamkumar.patel@epita.fr>
2021-12-21 12:22:32 +0100

    Edited cmake and cleaned up Skippex solution

--- SkippeX/Sources/main.cpp
+++ SkippeX/Sources/main.cpp
@@ -8,41 +8,106 @@
 // Standard Headers
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
+
+
+// Define Useful Variables and macros
+#define VSYNC GL_TRUE
+#define FULLSCREEN GL_FALSE
+int width = 1280;
+int height = 800;
+
+
+// Global GLFW Window
+GLFWwindow* window;
+GLFWmonitor* monitor;
+
+
+// Callbacks
+static void error_callback(int error, const char* description);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
 
 int main(int argc, char * argv[]) {
 
+    
     // Load GLFW and Create a Window
-    glfwInit();
+    if (!glfwInit()) {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
+
+    // Setup Monitor to Primary
+    monitor = glfwGetPrimaryMonitor();
+
+    // Handle case where we want to render at the maximum monitor resolution aka Fullscreen :-)
+    if (FULLSCREEN) {
+        const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+        width = mode->width;
+        height = mode->height;
+    }
+    std::cout << "Using Window Size : " << width << " x " << height << std::endl;
+
+    // Initialize Window
+    window = glfwCreateWindow(width, height, "Skippex | OpenGL", nullptr, nullptr);
+
 
     // Check for Valid Context
-    if (mWindow == nullptr) {
+    if (window == nullptr) {
         fprintf(stderr, "Failed to Create OpenGL Context");
         return EXIT_FAILURE;
     }
 
     // Create Context and Load OpenGL Functions
-    glfwMakeContextCurrent(mWindow);
+    glfwMakeContextCurrent(window);
     gladLoadGL();
     fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
 
+    // Setting up Callbacks
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetErrorCallback(error_callback);
+
+
+    // Enable or Disable VSYNC
+    glfwSwapInterval(VSYNC);
+
     // Rendering Loop
-    while (glfwWindowShouldClose(mWindow) == false) {
-        if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(mWindow, true);
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
 
         // Background Fill Color
         glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Flip Buffers and Draw
-        glfwSwapBuffers(mWindow);
+        glfwSwapBuffers(window);
         glfwPollEvents();
-    }   glfwTerminate();
-    return EXIT_SUCCESS;
+    }   
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
+
+
+// Callbacks
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
